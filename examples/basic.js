d02c83294abd5415fb1c0ba11c3a6e3786b39be1
Falco Nogatz <fnogatz@gmail.com>
2018-07-12 01:24:04 +0200

    Add output of ASCII chess board

--- examples/basic.js
+++ examples/basic.js
@@ -4,7 +4,9 @@ var board = new DGT.Board('/dev/ttyUSB0');
 board.on('ready', function() {
   console.log('Serial No:', board.serialNo);
   console.log('Version:', board.versionNo);
+  console.log(board.chess.ascii());
   console.log('-----');
+
 });
 
 board.on('data', function(data) {
