e6d2b88876479b0a36f17a380ad0229bf4233ffd
Falco Nogatz <fnogatz@gmail.com>
2018-07-25 18:53:53 +0200

    Use JavaScript standard style

--- examples/basic.js
+++ examples/basic.js
@@ -1,20 +1,18 @@
-var DGT = require('..');
-var board = new DGT.Board('/dev/ttyUSB0');
+var DGT = require('..')
+var board = new DGT.Board('/dev/ttyUSB0')
 
-board.on('ready', function() {
-  console.log('Serial No:', board.serialNo);
-  console.log('Version:', board.versionNo);
-  console.log(board.chess.ascii());
-  console.log('-----');
+board.on('ready', function () {
+  console.log('Serial No:', board.serialNo)
+  console.log('Version:', board.versionNo)
+  console.log(board.chess.ascii())
+  console.log('-----')
+})
 
-});
-
-board.on('data', function(data) {
-  console.log('Field:', data.field);
-  console.log('Piece:', data.piece);
-  console.log('-----');
-});
-
+board.on('data', function (data) {
+  console.log('Field:', data.field)
+  console.log('Piece:', data.piece)
+  console.log('-----')
+})
 
 /**
  * TODO:
