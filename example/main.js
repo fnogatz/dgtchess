2e0c952d5e500f9ec61d5d0da2f4ccdf6b24ea66
Falco Nogatz <fnogatz@gmail.com>
2020-12-27 22:36:17 +0100

    Add stream for changes on DGT board

--- example/main.js
+++ example/main.js
@@ -5,20 +5,47 @@ import Board from '../lib/Board.js'
 (function () {
   window.loadBoard = loadBoard
 
+  const serial = navigator.serial
   let board
   let chessground
 
-  function loadBoard () {
+  async function loadBoard () {
+    const port = await getPort()
+    if (!port) {
+      throw new Error('No port available.')
+    }
+    await port.open({ baudRate: 9600 })
+
+    board = new Board(port)
+    const { serialNr, version, position } = await board.reset()
+    document.getElementById('serialnr').innerHTML = serialNr
+    document.getElementById('version').innerHTML = version
+
     chessground = Chessground(document.getElementById('chessground'), {
       viewOnly: true,
       fen: '8/8/8/8/8/8/8/8/8'
     })
+    chessground.setPieces(position)
 
-    board = new Board()
-    board.addEventListener('ready', async function () {
-      document.getElementById('serialnr').innerHTML = board.serialNr
-      document.getElementById('version').innerHTML = board.version
-      chessground.setPieces(board.position)
+    const reader = board.readable.getReader()
+    while (true) {
+      const { value } = await reader.read()
+      chessground.setPieces(value)
+    }
+  }
+
+  async function getPort () {
+    const ports = await serial.getPorts()
+    for (const port of ports) {
+      for (const filter of Board.FILTERS) {
+        if (port.productId === filter.productId && port.vendorId === filter.vendorId) {
+          return port
+        }
+      }
+    }
+
+    return await serial.requestPort({
+      filters: Board.FILTERS
     })
   }
 })()
